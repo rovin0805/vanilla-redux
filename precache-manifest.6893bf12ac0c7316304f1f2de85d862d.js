7f563a4a19738e1fa6fb454db463e3d164f0f8c0
rovin0805 <qhdrn67@naver.com>
2020-12-05 18:27:44 +0900

    Updates

---
+++ precache-manifest.6893bf12ac0c7316304f1f2de85d862d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003005f9dc7ebe765ccc1d5a5af64ac3",
+    "url": "/vanilla-redux/index.html"
+  },
+  {
+    "revision": "1589f0b083185e84815d",
+    "url": "/vanilla-redux/static/js/2.8d407972.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vanilla-redux/static/js/2.8d407972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28dfc56ada88a5f631fe",
+    "url": "/vanilla-redux/static/js/main.7451ed35.chunk.js"
+  },
+  {
+    "revision": "350b489caad967ad910c",
+    "url": "/vanilla-redux/static/js/runtime-main.c8105fe6.js"
+  }
+]);
